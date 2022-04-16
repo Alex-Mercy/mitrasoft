b5dddbe16ebc00a5c9f74f5af826a830984a8bf9
Alex Mercy <hambiru@yandex.ru>
2022-04-16 21:07:19 +0300

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { Button, Container, Image, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import avatar from '../assets/images/avatar.png';
+import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap'
 
 function Header() {
     return (
 
+
+        <Navbar bg="dark" variant="dark" expand={false}>
+            <Container>
+                <Navbar.Brand href="/">
+                    <Link style={{ textDecoration: 'none' }} to='/'>
+                        <Image
+                            src={avatar}
+                            roundedCircle
+                            height='40'
+                        />
+
+                        <Navbar.Text className='mx-3'
+                        >Alex Mercy</Navbar.Text>
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-nav-dropdown" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav >
+                        <Navbar.Text
+                        >m-alex225@yandex.ru</Navbar.Text>
+                        <LinkContainer to='/'>
+                            <Nav.Link>
+                                About
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/galery'>
+                            <Nav.Link>
+                                Galery
+                            </Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
 
     )
 }
