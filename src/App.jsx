b2499e45d8f183e36f58ec26a5adcf8ffd1bd613
Alex Mercy <hambiru@yandex.ru>
2022-04-17 12:21:55 +0300

    categories, database

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import About from './pages/About';
 import Galery from './pages/Galery';
+import PictureDetails from './pages/PictureDetails.jsx';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             <Routes>
                 <Route path="/about" element={<About/>} />
                 <Route path="/" element={<Galery />} />
+                <Route path="/details" element={<PictureDetails />} />
             </Routes>
         </div>
 
--- src/pages/Galery.jsx
+++ src/pages/Galery.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-import { Card, Col, Container, Row } from 'react-bootstrap'
+import { Button, Card, Col, Container, Row } from 'react-bootstrap'
 
 function Galery() {
     return (
         <Container className="mt-3" >
+            <div className="my-3 px-2">
+            <Button className="mx-2" variant="outline-success">Animals</Button>
+            <Button className="mx-2" variant="outline-success">Animals</Button>
+            <Button className="mx-2" variant="outline-success">Animals</Button>
+            <Button className="mx-2" variant="outline-success">Animals</Button>
+            </div>
+            {/* <Row >
+                <Col xs lg="2"><Button>Animals</Button></Col>
+                <Col md="auto"><Button>Animals</Button></Col>
+                <Col><Button>Animals</Button></Col>
+                <Col><Button>Animals</Button></Col>
+                
+            </Row> */}
             <Row xs={1} lg={3} md={2} className="g-4" mx='10' >
                 {Array.from({ length: 6 }).map((_, idx) => (
                     <Col>
